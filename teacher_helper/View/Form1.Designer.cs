3a6082ebef1b906bb8c5a39360b3e4cf4fd38816
15305813298 <827073921@qq.com>
2020-05-18 19:28:47 +0800

    complete

--- teacher_helper/View/Form1.Designer.cs
+++ teacher_helper/View/Form1.Designer.cs
@@ -50,6 +50,7 @@
             // 
             this.textBox2.Location = new System.Drawing.Point(422, 164);
             this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
             this.textBox2.Size = new System.Drawing.Size(137, 21);
             this.textBox2.TabIndex = 1;
             // 
